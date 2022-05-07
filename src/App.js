fb1fc4b608ae557871dd6feea1a89d47c0d46240
unknown <ah3139892@gmail.com>
2022-05-07 08:32:41 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import logo from "./logo.svg";
 import HomePage from "./Pages/Home/HomePage";
 import MoviePage from "./Pages/Movie/MoviePage";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <>
-      {/* <Router>
+      <Router>
         <Routes>
           <Route path="/" element={<HomePage />} exact />
           <Route path="/movie/:id" element={<MoviePage />} />
         </Routes>
-      </Router> */}
-      <HomePage />
+      </Router>
     </>
   );
 }
